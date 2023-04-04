0832ec021f02b4fd310fb1eed567f8ba4e0a72da
Igor Danilovski <igor_danilovski@hotmail.com>
2023-04-04 15:03:44 +0200

    Initial Project Setup

---
+++ src/test/java/com/app/skillsify/SkillsifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.skillsify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
